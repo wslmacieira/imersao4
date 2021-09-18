f6fc692718eb9505e2618725fcc835f038adf53a
wslmacieira <wslmacieira@outlook.com>
2021-09-18 16:43:23 -0300

    update transaction entity

--- nestjs-api/src/transactions/transactions.module.ts
+++ nestjs-api/src/transactions/transactions.module.ts
@@ -1,8 +1,11 @@
+import { Transaction } from './entities/transaction.entity';
+import { SequelizeModule } from '@nestjs/sequelize';
 import { Module } from '@nestjs/common';
 import { TransactionsService } from './transactions.service';
 import { TransactionsController } from './transactions.controller';
 
 @Module({
+  imports: [SequelizeModule.forFeature([Transaction])],
   controllers: [TransactionsController],
   providers: [TransactionsService]
 })
