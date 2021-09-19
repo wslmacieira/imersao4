4995fcb7c3450966fd0e0c04398603531305aa17
wslmacieira <wslmacieira@outlook.com>
2021-09-19 08:07:12 -0300

    configure theme

--- nextjs-frontend/src/pages/_app.tsx
+++ nextjs-frontend/src/pages/_app.tsx
@@ -1,7 +1,20 @@
-import '../styles/globals.css'
-import type { AppProps } from 'next/app'
+import "../styles/globals.css";
+import type { AppProps } from "next/app";
+import { ThemeProvider } from "@material-ui/styles";
+import theme from "../utils/theme";
+import { CssBaseline } from "@material-ui/core";
+import { useEffect } from "react";
 
 function MyApp({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  useEffect(() => {
+    const jssStyles = document.querySelector("#jss-server-side");
+    jssStyles?.parentElement?.removeChild(jssStyles);
+  }, []);
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
-export default MyApp
+export default MyApp;
